432f1004a9389d73d04dc654625e8a0bc6c9e840
JinhaoLiu <jinhaoliu2018@gmail.com>
2018-03-15 17:06:40 -0700

    fix bugs

---
+++ src/app/app.routing.ts
@@ -0,0 +1,43 @@
+import {Routes, RouterModule} from '@angular/router';
+
+import {RegisterComponent} from './views/user/register/register.component';
+import {LoginComponent} from './views/user/login/login.component';
+import {ProfileComponent} from './views/user/profile/profile.component';
+import {WebsiteListComponent} from './views/website/website-list/website-list.component';
+import {WebsiteNewComponent} from './views/website/website-new/website-new.component';
+import {WebsiteEditComponent} from './views/website/website-edit/website-edit.component';
+import {PageListComponent} from './views/pages/page-list/page-list.component';
+import {PageNewComponent} from './views/pages/page-new/page-new.component';
+import {PageEditComponent} from './views/pages/page-edit/page-edit.component';
+import {WidgetListComponent} from './views/widget/widget-list/widget-list.component';
+import {WidgetChooserComponent} from './views/widget/widget-chooser/widget-chooser.component';
+import {WidgetEditComponent} from './views/widget/widget-edit/widget-edit.component';
+import {WidgetHeaderComponent} from './views/widget/widget-edit/widget-header/widget-header.component';
+import {WidgetImageComponent} from './views/widget/widget-edit/widget-image/widget-image.component';
+import {WidgetYoutubeComponent} from './views/widget/widget-edit/widget-youtube/widget-youtube.component';
+import {WidgetHtmlComponent} from './views/widget/widget-edit/widget-html/widget-html.component';
+import {WidgetTextComponent} from './views/widget/widget-edit/widget-text/widget-text.component';
+
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user/:_id', component: ProfileComponent},
+  {path: 'user/:_id/website', component: WebsiteListComponent},
+  {path: 'user/:_id/website/new', component: WebsiteNewComponent},
+  {path: 'user/:_id/website/:wid', component: WebsiteEditComponent},
+  {path: 'user/:uid/website/:wid/page', component: PageListComponent},
+  {path: 'user/:uid/website/:wid/page/new', component: PageNewComponent},
+  {path: 'user/:uid/website/:wid/page/:pid', component: PageEditComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget', component: WidgetListComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/new', component: WidgetChooserComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/header', component: WidgetHeaderComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/image', component: WidgetImageComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/youtube', component: WidgetYoutubeComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/html', component: WidgetHtmlComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/text', component: WidgetTextComponent},
+  {path: 'user/:uid/website/:wid/page/:pid/widget/:wgid', component: WidgetEditComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
