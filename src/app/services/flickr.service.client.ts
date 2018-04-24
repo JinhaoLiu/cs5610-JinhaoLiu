04ab3e27b81152e2ab34848dbbc43404f84fad1d
JinhaoLiu <jinhaoliu2018@gmail.com>
2018-04-23 23:03:17 -0700

    test

--- src/app/services/flickr.service.client.ts
+++ src/app/services/flickr.service.client.ts
@@ -1,19 +1,19 @@
 import {Http} from '@angular/http';
 import {Injectable} from '@angular/core';
 
-@Injectable() // needed as we're injecting Http service into this service
+@Injectable()
 export class FlickrService {
 
-  key = '9e1b1e0b370f1bd3b232741ac7cffdee';
-  secret = '3c47ba6d2c50b285';
+  key = 'fc38f740eca2dee8babd800c49eb559c';
+  secret = '7562619adfaa4284';
   urlBase = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT';
 
   constructor(private http: Http) {}
 
   searchPhotos(searchTerm: any) {
-  const url = this.urlBase
-    .replace('API_KEY', this.key)
-    .replace('TEXT', searchTerm);
+    const url = this.urlBase
+      .replace('API_KEY', this.key)
+      .replace('TEXT', searchTerm);
     return this.http.get(url);
   }
 }
